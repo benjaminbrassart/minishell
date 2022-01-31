7388c32c5978ee7a66ec26f9560fa539622c2cc2
Benjamin BRASSART <bbrassar@student.42.fr>
2022-01-31 09:46:15 +0100

    Started path_search, memory leaks and path normalization to do

--- include/environ.h
+++ include/environ.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 10:13:56 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/12 11:45:09 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/01/31 08:44:30 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include "type/env.h"
 # include "type/sh.h"
 
+# define PATH_VAR_NAME "PATH"
+
 int		env_init(t_sh *sh, char *ev[]);
 
 t_env	*env_newent(char *key, char *value);
@@ -23,5 +25,7 @@ t_env	*env_newent(char *key, char *value);
 void	env_destroy(t_sh *sh);
 
 char	*env_get(t_sh *sh, char const *key);
+
+char	*path_search(t_sh *sh, char *bin_name);
 
 #endif
--- src/util/path_search.c
+++ src/util/path_search.c
@@ -6,18 +6,45 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:40:06 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/12 11:45:30 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/01/31 09:28:36 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "environ.h"
+#include "ft.h"
+#include <dirent.h>
+#include <stdio.h>
 
-char	*path_search(t_sh *sh, char *bin_rel)
+char	*path_search(t_sh *sh, char *bin_name)
 {
-	t_env	*env;
-	char	*raw_path;
+	char *const		path = env_get(sh, PATH_VAR_NAME);
+	char			**path_entries;
+	size_t			n;
+	DIR				*dir;
+	struct dirent	*ent;
 
-	raw_path = env_get(sh, "PATH");
-	if (!raw_path)
-	env = sh->env;
+	if (!path)
+		return (NULL);
+	path_entries = ft_split(path, ':');
+	if (!path_entries)
+		return (NULL);
+	n = 0;
+	while (path_entries[n])
+	{
+		dir = opendir(path_entries[n]);
+		if (dir)
+		{
+			while (1)
+			{
+				ent = readdir(dir);
+				if (!ent)
+					break ;
+				if (ft_strcmp(bin_name, ent->d_name) == 0)
+					return (ft_strjoin(path_entries[n], ent->d_name));
+			}
+			closedir(dir);
+		}
+		++n;
+	}
+	return (NULL);
 }
