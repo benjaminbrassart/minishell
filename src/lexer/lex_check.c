ed7ffddeaaf68677c0fd974073c424685241c37a
Benjamin BRASSART <bbrassar@student.42.fr>
2022-01-31 04:47:48 +0100

    Added new WORD-type tokens to lex_check (TODO)

--- src/lexer/lex_check.c
+++ src/lexer/lex_check.c
@@ -6,19 +6,25 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 05:55:58 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/21 06:13:22 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/01/22 07:25:57 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 #include "minishell.h"
 #include <stdio.h>
+#include <unistd.h>
 
 // FIXME magic value
 static int	print_error(t_token_node *node)
 {
 	g_exit_status = 2;
 	return (0);
+}
+
+static int	is_word(t_token token)
+{
+	return (token == WORD_DQ || token == WORD_NQ || token == WORD_SQ);
 }
 
 int	lex_check(t_token_list *list)
@@ -28,9 +34,10 @@ int	lex_check(t_token_list *list)
 	node = list->first_node;
 	while (node)
 	{
-		if (node->token == PIPE && node == list->first_node)
+		if ((node == list->first_node && node->token == PIPE)
+			|| (node == list->last_node && !is_word(node->token)))
 			return (print_error(node));
-
 		node = node->next;
 	}
+	return (1);
 }
