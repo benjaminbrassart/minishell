6b0a457912d54c27f634cb36dec735390b944a95
Benjamin BRASSART <bbrassar@student.42.fr>
2022-01-31 09:44:29 +0100

    Changed t_token for bit-by-bit checking

--- include/type/token.h
+++ include/type/token.h
@@ -6,26 +6,27 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 10:29:25 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/19 16:48:07 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/01/31 08:38:59 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKEN_H
 # define TOKEN_H
 
-typedef enum e_token	t_token;
+typedef int	t_token;
 
 enum e_token
 {
-	WORD_NQ,
-	WORD_SQ,
-	WORD_DQ,
-	PIPE,
-	LESS,
-	D_LESS,
-	GREAT,
-	D_GREAT,
-	SEPARATOR,
+	WORD_NQ = 1 << 0,
+	WORD_SQ = 1 << 1,
+	WORD_DQ = 1 << 2,
+	WORD = WORD_NQ | WORD_SQ | WORD_SQ,
+	PIPE = 1 << 3,
+	LESS = 1 << 4,
+	D_LESS = 1 << 5,
+	GREAT = 1 << 6,
+	D_GREAT = 1 << 7,
+	SEPARATOR = 1 << 8,
 };
 
 #endif
