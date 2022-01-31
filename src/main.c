ff357f2147623fc69cceb5a5654126691575fb27
Benjamin BRASSART <bbrassar@student.42.fr>
2022-01-31 04:48:04 +0100

    Added primary builtin check

--- src/main.c
+++ src/main.c
@@ -6,10 +6,11 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 23:23:10 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/22 11:47:56 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/01/22 13:02:47 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "builtin.h"
 #include "environ.h"
 #include "lexer.h"
 #include "minishell.h"
@@ -21,7 +22,19 @@
 
 static void	process_line(t_sh *sh, char *line)
 {
+	t_token const	token = sh->tokens.first_node->token;
+	t_builtin const	*builtin;
+
 	add_history(line);
+	if (token == WORD_NQ || token == WORD_SQ || token == WORD_DQ)
+	{
+		builtin = get_builtin(sh->tokens.first_node->value);
+		if (builtin)
+		{
+			printf("builtin `%s'\n", builtin->name);
+			builtin->fn(sh->env);
+		}
+	}
 	free(line);
 	lex_dump(&sh->tokens);
 	lex_delete(&sh->tokens);
