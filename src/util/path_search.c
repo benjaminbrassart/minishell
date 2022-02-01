1b04e3bced5974f0f631e999776d0ba99e4ff1dc
Benjamin BRASSART <bbrassar@student.42.fr>
2022-02-01 08:57:44 +0100

    Added path searching

--- src/util/path_search.c
+++ src/util/path_search.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 11:40:06 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/31 09:28:36 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/01 08:49:21 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,59 @@
 #include "ft.h"
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-char	*path_search(t_sh *sh, char *bin_name)
+static char	*path_join(char *dir_name, char *bin_name)
 {
-	char *const		path = env_get(sh, PATH_VAR_NAME);
-	char			**path_entries;
-	size_t			n;
-	DIR				*dir;
-	struct dirent	*ent;
+	char			*buffer;
+	int				add_slash;
+	size_t const	dir_len = ft_strlen(dir_name);
+	size_t const	bin_len = ft_strlen(bin_name);
 
-	if (!path)
-		return (NULL);
-	path_entries = ft_split(path, ':');
-	if (!path_entries)
-		return (NULL);
-	n = 0;
-	while (path_entries[n])
+	add_slash = 1;
+	if (dir_name[dir_len] == '/')
+		add_slash = 0;
+	buffer = malloc(sizeof (*buffer) * (dir_len + bin_len + add_slash + 1));
+	if (buffer)
 	{
-		dir = opendir(path_entries[n]);
-		if (dir)
-		{
-			while (1)
-			{
-				ent = readdir(dir);
-				if (!ent)
-					break ;
-				if (ft_strcmp(bin_name, ent->d_name) == 0)
-					return (ft_strjoin(path_entries[n], ent->d_name));
-			}
-			closedir(dir);
-		}
+		ft_memmove(buffer, dir_name, dir_len);
+		ft_memmove(buffer + dir_len, "/", add_slash);
+		ft_memmove(buffer + dir_len + add_slash, bin_name, bin_len);
+		buffer[dir_len + add_slash + bin_len] = 0;
+	}
+	return (buffer);
+}
+
+static char	*path_search_entries(char **entries, char *bin_name)
+{
+	size_t	n;
+	char	*path;
+
+	n = 0;
+	while (entries[n])
+	{
+		path = path_join(entries[n], bin_name);
+		if (path && access(path, X_OK) == 0)
+			return (path);
+		free(path);
 		++n;
 	}
 	return (NULL);
+}
+
+char	*path_search(t_sh *sh, char *bin_name)
+{
+	char *const	path_var = env_get(sh, PATH_VAR_NAME);
+	char		**path_entries;
+	char		*exec;
+
+	if (!path_var)
+		return (NULL);
+	path_entries = ft_split(path_var, ':');
+	if (!path_entries)
+		return (NULL);
+	exec = path_search_entries(path_entries, bin_name);
+	ft_split_destroy(path_entries);
+	return (exec);
 }
