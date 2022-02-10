7d9e68cc0f3f8bde36d746975fdaa179a2c04b6a
Benjamin BRASSART <bbrassar@student.42.fr>
2022-02-11 00:39:46 +0100

    Added builtin env

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,18 +6,26 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 12:28:10 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/02/08 02:50:58 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/11 00:39:00 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+#include <stdio.h>
 
 int	builtin_env(
 	int argc __attribute__((unused)),
 	char *argv[] __attribute__((unused)),
-	t_env_table *env __attribute__((unused))
+	t_env_table *env
 )
 {
-	(void)env;
+	t_env	*entry;
+
+	entry = env->first_entry;
+	while (entry)
+	{
+		printf("%s=%s\n", entry->key, entry->value);
+		entry = entry->next;
+	}
 	return (0);
 }
