a5932ae38290e522e0ab610661b1e8b766c639de
Benjamin BRASSART <bbrassar@student.42.fr>
2022-02-11 00:28:15 +0100

    Fixed non-initialized bytes in setup

--- src/setup.c
+++ src/setup.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 09:24:19 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/02/05 12:22:36 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/11 00:15:42 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static void	setup_signal_handlers(void)
 	sa_quit.sa_handler = SIG_IGN;
 	sigemptyset(&sa_int.sa_mask);
 	sigemptyset(&sa_quit.sa_mask);
+	sa_int.sa_flags = 0;
+	sa_quit.sa_flags = 0;
 	sigaction(SIGINT, &sa_int, NULL);
 	sigaction(SIGQUIT, &sa_quit, NULL);
 }
