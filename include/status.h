587fdf53825dc0bad8b0839eee16e83af9f9a7e2
Benjamin BRASSART <bbrassar@student.42.fr>
2022-02-11 01:18:37 +0100

    Added exit status constants

--- include/status.h
+++ include/status.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:35:22 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/02/08 03:52:45 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/11 01:12:44 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define STATUS_H
 
 # include <signal.h>
+
+# define EXIT_STATUS_OK 0
+# define EXIT_STATUS_MINOR 1
+# define EXIT_STATUS_MAJOR 2
+# define EXIT_STATUS_MAX 256
 
 /* Content of $? */
 static volatile int	g_exit_status;
