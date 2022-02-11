5692aece5be2fd461794b1f9471d6a8512e603db
Benjamin BRASSART <bbrassar@student.42.fr>
2022-02-11 01:19:34 +0100

    Improved builtin cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,13 +6,14 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 04:14:06 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/02/08 02:49:37 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/11 01:15:00 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "buffer.h"
 #include "builtin.h"
 #include "minishell.h"
+#include "status.h"
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
@@ -25,13 +26,13 @@ int	builtin_cd(
 {
 	if (argc == 2)
 	{
-		if (chdir(argv[1]) != 0)
-			return (builtin_error(BUILTIN_CD, strerror(errno)), 1);
-		return (0);
+		if (chdir(argv[1]) == 0)
+			return (EXIT_STATUS_OK);
+		builtin_error(BUILTIN_CD, strerror(errno));
 	}
-	if (argc < 2)
+	else if (argc < 2)
 		builtin_error(BUILTIN_CD, "not enough arguments");
-	else
+	else if (argc > 2)
 		builtin_error(BUILTIN_CD, "too many arguments");
-	return (1);
+	return (EXIT_STATUS_MINOR);
 }
