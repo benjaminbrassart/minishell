548605f0c8c2ef97a9e3925fe44ae2977eb6a2dd
Benjamin BRASSART <bbrassar@student.42.fr>
2022-02-15 02:10:50 +0100

    Shortened utility function in export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 13:09:09 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/02/14 14:09:29 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/15 02:08:15 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include "status.h"
 #include <errno.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 
 static int	is_var_name(char const *s)
 {
@@ -24,9 +26,7 @@ static int	is_var_name(char const *s)
 	n = 0;
 	while (s[n] && s[n] != '=')
 	{
-		if (!ft_isalnum(s[n]) && s[n] != '_')
-			return (0);
-		if (n == 0 && ft_isdigit(s[n]))
+		if ((!ft_isalnum(s[n]) && s[n] != '_') || (n == 0 && ft_isdigit(s[n])))
 			return (0);
 		++n;
 	}
@@ -40,7 +40,7 @@ int	builtin_export(int argc, char *argv[], t_env_table *env)
 	t_env	*entry;
 
 	if (argc < 2)
-		return (builtin_error(BUILTIN_EXPORT, "not enough arguments"), 1);
+		return (builtin_error(BUILTIN_EXPORT, "Not enough arguments"), 1);
 	res = 0;
 	i = 1;
 	while (i < argc)
