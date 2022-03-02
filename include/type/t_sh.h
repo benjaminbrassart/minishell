aa562a67118500df5beb290d81e56a72f05e6a8d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-03-02 18:52:58 +0100

    Updated main

--- include/type/t_sh.h
+++ include/type/t_sh.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 10:28:08 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/02/08 01:48:13 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/02/15 06:26:12 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ struct s_sh
 {
 	t_token_list	tokens;
 	t_env_table		env;
+	char const		*prompt;
 };
 
 #endif
