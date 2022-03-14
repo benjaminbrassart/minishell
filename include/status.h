58df640db382bd50d8b5be0a251f2dec62cd4e9d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-03-14 18:04:23 +0100

    Added missing macro definiton EXIT_STATUS_EXEC_DIR

--- include/status.h
+++ include/status.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:35:22 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/03/09 00:16:15 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/03/14 17:39:29 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define EXIT_STATUS_OK 0
 # define EXIT_STATUS_MINOR 1
 # define EXIT_STATUS_MAJOR 2
+# define EXIT_STATUS_EXEC_DIR 126
 # define EXIT_STATUS_NOT_FOUND 127
 # define EXIT_STATUS_SIGNAL_OFFSET 128
 # define EXIT_STATUS_MAX 256
