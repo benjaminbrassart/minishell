8c0604a1be5c8975914124be9d24a2fa59140bd2
Benjamin BRASSART <bbrassar@student.42.fr>
2022-03-18 18:11:09 +0100

    Added empty line check Empty lines do not longer update history

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 23:23:10 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/03/18 15:13:12 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/03/18 18:08:43 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@
 #include <unistd.h>
 
 int	g_exit_status;
+
+static int	is_line_empty(char const *line)
+{
+	while (*line && ft_isspace(*line))
+		++line;
+	return (*line == 0);
+}
 
 static void	process_line(t_sh *sh)
 {
@@ -74,15 +81,18 @@ int	main(
 		line = readline(DEFAULT_PROMPT);
 		if (!line)
 			break ;
-		if (lex_tokenize(&sh.tokens, line)
-			&& lex_heredoc(&sh.tokens, &sh.heredoc)
-			&& lex_expand(&sh.tokens, &sh.env)
-			&& lex_postexpand(&sh.tokens)
-			&& lex_check_syntax(&sh.tokens) && sh.tokens.length > 0)
-			process_line(&sh);
-		add_history(line);
-		lex_delete(&sh.tokens);
-		lex_heredoc_delete(&sh.heredoc);
+		if (!is_line_empty(line))
+		{
+			if (lex_tokenize(&sh.tokens, line)
+				&& lex_heredoc(&sh.tokens, &sh.heredoc)
+				&& lex_expand(&sh.tokens, &sh.env)
+				&& lex_postexpand(&sh.tokens)
+				&& lex_check_syntax(&sh.tokens) && sh.tokens.length > 0)
+				process_line(&sh);
+			add_history(line);
+			lex_delete(&sh.tokens);
+			lex_heredoc_delete(&sh.heredoc);
+		}
 		free(line);
 	}
 	return (process_end(&sh));
