d4ea30123bd2169638ac9ecd895f7a451962cfd6
Benjamin BRASSART <bbrassar@student.42.fr>
2022-03-22 05:19:26 +0100

    Refractored exec_redirect

--- src/executor/exec_redirect.c
+++ src/executor/exec_redirect.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 23:42:58 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/03/22 00:17:50 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/03/22 05:18:56 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,26 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
+
+static void	_red_in(t_exec_red *red, t_exec_red **last_heredoc)
+{
+	if (red->type & D_LESS)
+	{
+		if (last_heredoc)
+			close((*last_heredoc)->fd);
+		*last_heredoc = red;
+	}
+	if (red->exec->fd_in != STDIN_FILENO)
+		close(red->exec->fd_in);
+	red->exec->fd_in = red->fd;
+}
+
+static void	_red_out(t_exec_red *red)
+{
+	if (red->exec->fd_out != STDOUT_FILENO)
+		close(red->exec->fd_out);
+	red->exec->fd_out = red->fd;
+}
 
 int	exec_redirect(t_exec *exec)
 {
@@ -30,23 +50,9 @@ int	exec_redirect(t_exec *exec)
 		if (!open_red(red))
 			break ;
 		if (red->type & RED_IN)
-		{
-			if (red->type & D_LESS)
-			{
-				if (last_heredoc)
-					close(last_heredoc->fd);
-				last_heredoc = red;
-			}
-			if (exec->fd_in != STDIN_FILENO)
-				close(exec->fd_in);
-			exec->fd_in = red->fd;
-		}
+			_red_in(red, &last_heredoc);
 		else if (red->type & RED_OUT)
-		{
-			if (exec->fd_out != STDOUT_FILENO)
-				close(exec->fd_out);
-			exec->fd_out = red->fd;
-		}
+			_red_out(red);
 		red = red->next;
 	}
 	exec_delete_redirect(exec->meta);
