caa51a9b1fbe625dbfb42f5f0b62831c10aeb4bd
Benjamin BRASSART <bbrassar@student.42.fr>
2022-03-24 05:25:39 +0100

    Refractored lex_heredoc_read

--- src/lexer/heredoc/lex_heredoc_read.c
+++ src/lexer/heredoc/lex_heredoc_read.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 08:14:45 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/03/23 01:39:42 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/03/24 05:30:07 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,40 +31,57 @@ static void	heredoc_read_eof(char const *delimiter)
 	buffer_delete(&buffer);
 }
 
+static void	_delim(char const *line, char *delimiter, int fd)
+{
+	if (line)
+		close(STDIN_FILENO);
+	else
+		heredoc_read_eof(delimiter);
+	free(delimiter);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+}
+
+static int	_loop(t_heredoc_buffer *hdbuf, int fd)
+{
+	char	*line;
+	int		res;
+
+	res = 1;
+	while (res)
+	{
+		line = readline(HEREDOC_PROMPT);
+		if (line && ft_strcmp(hdbuf->delimiter, line) != 0)
+		{
+			if (!buffer_append(&hdbuf->buffer, line)
+				|| !buffer_cappend(&hdbuf->buffer, '\n'))
+			{
+				buffer_delete(&hdbuf->buffer);
+				res = 0;
+			}
+		}
+		else
+		{
+			_delim(line, hdbuf->delimiter, fd);
+			free(line);
+			break ;
+		}
+		free(line);
+	}
+	return (res);
+}
+
 int	lex_heredoc_read(t_heredoc *heredoc)
 {
 	size_t	n;
 	int		fd;
-	char	*line;
 
 	n = 0;
 	while (n < heredoc->count)
 	{
 		fd = dup(STDIN_FILENO);
-		if (fd == -1)
+		if (fd == -1 || !_loop(&heredoc->buffers[n], fd))
 			return (0);
-		while (1)
-		{
-			line = readline(HEREDOC_PROMPT);
-			if (line && ft_strcmp(heredoc->buffers[n].delimiter, line) != 0)
-			{
-				if (!buffer_append(&heredoc->buffers[n].buffer, line)
-					|| !buffer_cappend(&heredoc->buffers[n].buffer, '\n'))
-					return (buffer_delete(&heredoc->buffers[n].buffer), free(line), 0);
-			}
-			else
-			{
-				if (line)
-					close(STDIN_FILENO);
-				else
-					heredoc_read_eof(heredoc->buffers[n].delimiter);
-				free(heredoc->buffers[n].delimiter);
-				dup2(fd, STDIN_FILENO);
-				close(fd);
-				break ;
-			}
-			free(line);
-		}
 		if (!buffer_flush(&heredoc->buffers[n].buffer))
 			return (buffer_delete(&heredoc->buffers[n].buffer), 0);
 		++n;
