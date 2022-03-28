21bdc6f403e09b33b002cb9364da520dcb77941f
Benjamin BRASSART <bbrassar@student.42.fr>
2022-03-28 14:44:49 +0200

    Removed color reset in prompt

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 00:06:36 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/03/27 04:05:42 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/03/28 14:44:25 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "type/t_sh.h"
 
 /* The prompt to show when waiting for a command */
-# define DEFAULT_PROMPT "\033[0m$> "
+# define DEFAULT_PROMPT "$> "
 # define PROGRAM_NAME "minishell"
 # define EXIT_MESSAGE "exit"
 
