be7fb21e6cdd9ed23d475aed85d2a4e7a84fe7b8
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-01 00:51:52 +0200

    Added `--trace-children=yes` to valgrind options

--- mk/valgrind.mk
+++ mk/valgrind.mk
@@ -6,7 +6,7 @@
 #    By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/02 16:12:07 by bbrassar          #+#    #+#              #
-#    Updated: 2022/03/02 17:19:53 by bbrassar         ###   ########.fr        #
+#    Updated: 2022/03/31 21:00:30 by bbrassar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,7 @@ valgrind:
 					--suppressions=readline.supp \
 					--leak-check=full \
 					--show-leak-kinds=all \
+					--trace-children=yes \
 					--track-fds=yes \
 					./$(NAME)
 
