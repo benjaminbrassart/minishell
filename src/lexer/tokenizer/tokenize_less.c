f21a13d18b4b38f4387e513a979f2d03bd5548ba
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-01 06:31:26 +0200

    Set token value to NULL instead of an empty string for non-word tokenizers

--- src/lexer/tokenizer/tokenize_less.c
+++ src/lexer/tokenizer/tokenize_less.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 11:37:31 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/01/31 12:55:31 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/04/01 04:19:27 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int	tokenize_less(t_token_list *list, char **input)
 {
-	return (lex_add_token(list, (t_token_param){LESS, ++(*input), 0}));
+	++(*input);
+	return (lex_add_token(list, (t_token_param){LESS, NULL, 0}));
 }
