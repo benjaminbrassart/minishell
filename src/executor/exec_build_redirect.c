8c31675618e357e2c758b759d2fb2801f49fcf42
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-01 03:36:08 +0200

    Fixed exec_build_redirect always redirecting on first child

--- src/executor/exec_build_redirect.c
+++ src/executor/exec_build_redirect.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 06:13:37 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/03/23 04:12:55 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/04/01 03:32:23 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,22 +44,20 @@ static int	add_red(t_exec *exec, char *path, int type, int hd_index)
 	return (1);
 }
 
-static int	_loop(t_token_node **node, t_exec_meta *meta, int *hd_index)
+static int	_loop(t_token_node **node, t_exec_meta *meta, int *hd_index,
+size_t *n)
 {
-	size_t	n;
-
-	n = 0;
 	if ((*node)->token == PIPE)
-		++n;
+		++(*n);
 	else if ((*node)->token & (RED_OUT | LESS))
 	{
-		if (!add_red(&meta->exec[n], (*node)->next->value, (*node)->token, -1))
+		if (!add_red(&meta->exec[*n], (*node)->next->value, (*node)->token, -1))
 			return (0);
 		*node = (*node)->next;
 	}
 	else if ((*node)->token == D_LESS)
 	{
-		if (!add_red(&meta->exec[n], NULL, D_LESS, (*hd_index)++))
+		if (!add_red(&meta->exec[*n], NULL, D_LESS, (*hd_index)++))
 			return (0);
 		*node = (*node)->next;
 	}
@@ -73,11 +71,13 @@ int	exec_build_redirect(t_exec_meta *meta)
 	t_token_list const	*list = &meta->sh->tokens;
 	t_token_node		*node;
 	int					hd_index;
+	size_t				n;
 
 	node = list->first_node;
 	hd_index = 0;
+	n = 0;
 	while (node)
-		if (!_loop(&node, meta, &hd_index))
+		if (!_loop(&node, meta, &hd_index, &n))
 			return (0);
 	return (1);
 }
