caa96c00fc6eb888403dd6d480a6a0ddfde95300
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-01 06:32:11 +0200

    Added colors to valgrind Makefile

--- mk/valgrind.mk
+++ mk/valgrind.mk
@@ -6,17 +6,21 @@
 #    By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/02 16:12:07 by bbrassar          #+#    #+#              #
-#    Updated: 2022/03/31 21:00:30 by bbrassar         ###   ########.fr        #
+#    Updated: 2022/04/01 03:00:24 by bbrassar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-valgrind:
-					@valgrind \
+VALGRIND			= valgrind
+VALGRIND_OPT		= \
 					--suppressions=readline.supp \
 					--leak-check=full \
 					--show-leak-kinds=all \
 					--trace-children=yes \
-					--track-fds=yes \
-					./$(NAME)
+					--track-fds=yes
+EXC_NAME			= $(dir $(NAME))$(notdir $(NAME))
+
+valgrind:
+					@echo -e '\033[33m$(VALGRIND) \033[36m$(VALGRIND_OPT) \033[0m-- $(EXC_NAME)'
+					@$(VALGRIND) $(VALGRIND_OPT) -- $(EXC_NAME)
 
 .PHONY:				valgrind
