a9c993bfb4a51f852ef23aab3dbb306c13b6d8bb
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-04 11:48:52 +0200

    Fixed ft_strpbrk

--- libft/ft_strpbrk.c
+++ libft/ft_strpbrk.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 02:31:39 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/04/04 02:45:29 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/04/04 11:47:31 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strpbrk(char const *s, char const *accept)
 	char const	*p;
 
 	p = s;
-	while (*p != 0 && ft_strchr(accept, *p) != NULL)
+	while (*p != 0 && ft_strchr(accept, *p) == NULL)
 		++p;
 	if (*p == 0)
 		p = NULL;
