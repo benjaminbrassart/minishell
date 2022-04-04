74cf754669d1ba8a6f8124e899249eeea2f8b6fa
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-04 02:45:41 +0200

    Changed ft_split to accept a set of bytes to split Added ft_strpbrk Rearranged ft.h

--- libft/ft.h
+++ libft/ft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msainton <msainton@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 17:04:23 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/04/02 15:34:18 by msainton         ###   ########.fr       */
+/*   Updated: 2022/04/04 02:43:23 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,53 +17,55 @@
 
 # include <stddef.h>
 
-int				ft_strcmp(char const *s1, char const *s2);
+int		ft_strcmp(char const *s1, char const *s2);
 
-size_t			ft_strcspn(char const *s, char const *reject);
+size_t	ft_strcspn(char const *s, char const *reject);
 
-char			*ft_strdup(char const *s)
-				__attribute__((malloc));
+char	*ft_strpbrk(char const *s, char const *accept);
 
-char			*ft_strndup(char const *s, size_t n)
-				__attribute__((malloc));
+char	*ft_strdup(char const *s)
+		__attribute__((malloc));
 
-char			*ft_strncat(char *dst, char const *src, size_t n);
+char	*ft_strndup(char const *s, size_t n)
+		__attribute__((malloc));
 
-void			*ft_memset(void *dst, int c, size_t n);
+char	*ft_strncat(char *dst, char const *src, size_t n);
 
-void			*ft_memmove(void *dst, void const *src, size_t n);
+void	*ft_memset(void *dst, int c, size_t n);
 
-size_t			ft_strlen(char const *s);
+void	*ft_memmove(void *dst, void const *src, size_t n);
 
-char			*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(char const *s);
 
-int				ft_isalnum(int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
-int				ft_isalpha(int c);
+int		ft_isalnum(int c);
 
-int				ft_isspace(int c);
+int		ft_isalpha(int c);
 
-int				ft_isdigit(int c);
+int		ft_isspace(int c);
 
-char			**ft_split(char const *s, int c);
+int		ft_isdigit(int c);
 
-void			*ft_split_destroy(char **array);
+char	**ft_split(char const *s, char const *set);
 
-int				ft_strncmp(char const *s1, char const *s2, size_t n);
+void	*ft_split_destroy(char **array);
 
-char			*ft_itoa(int i)
-				__attribute__((malloc));
+int		ft_strncmp(char const *s1, char const *s2, size_t n);
 
-char			*ft_strchr(char const *s, int c);
+char	*ft_itoa(int i)
+		__attribute__((malloc));
 
-void			*ft_calloc(size_t n, size_t size)
-				__attribute__((malloc));
+char	*ft_strchr(char const *s, int c);
 
-char			*ft_strtrim(char const *s)
-				__attribute__((malloc));
+void	*ft_calloc(size_t n, size_t size)
+		__attribute__((malloc));
 
-int				ft_strtoi(char const *s, char **endptr);
+char	*ft_strtrim(char const *s)
+		__attribute__((malloc));
 
-int				get_next_line(int fd, char **line);
+int		ft_strtoi(char const *s, char **endptr);
+
+int		get_next_line(int fd, char **line);
 
 #endif
---
+++ libft/ft_strpbrk.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strpbrk.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 02:31:39 by bbrassar          #+#    #+#             */
+/*   Updated: 2022/04/04 02:45:29 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+
+char	*ft_strpbrk(char const *s, char const *accept)
+{
+	char const	*p;
+
+	p = s;
+	while (*p != 0 && ft_strchr(accept, *p) != NULL)
+		++p;
+	if (*p == 0)
+		p = NULL;
+	return ((char *)p);
+}
